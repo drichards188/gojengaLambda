610297547ab1d16ad934a44928e4ff29273e28bb
drichards188 <github@cxoprotect.com>
2023-11-02 11:14:35 -0600

    hardcoded risk returns to 5 coins

--- app/routes/risk_router.py
+++ app/routes/risk_router.py
@@ -16,6 +16,18 @@ async def get_sharpe_ratio(request: Request, symbol: str):
     try:
         sharpe_ratio: float = RiskHandler.handle_get_sharpe_ratio(symbol)
         risk: object = RiskHandler.evaluate_sharpe_ratio(sharpe_ratio)
+
+        if symbol == "btc":
+            risk = {"ratio": 1.18, "eval": "Adequate"}
+        elif symbol == "eth":
+            risk = {"ratio": 0.27, "eval": "Bad"}
+        elif symbol == "xrp":
+            risk = {"ratio": 0.39, "eval": "Bad"}
+        elif symbol == "usdt":
+            risk = {"ratio": 0.01, "eval": "Bad"}
+        elif symbol == "bnb":
+            risk = {"ratio": -0.87, "eval": "Bad"}
+
         print(f'--> risk is: {risk}')
         return {"response": risk}
     except Exception as e:
